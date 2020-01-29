adb562a1c4796c7c691802dcf718cf61fbfce4cf
Sabin Ionut <sabin_balan@yahoo.com>
2020-01-29 22:04:55 +0200

    add revision

---
+++ BE.DAL/Client.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace BE.DAL
+{
+    public class Client
+    {
+        public int ClientId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+                 
+    }
+}
---
+++ BE.DAL/Revision.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BE.DAL
+{
+    public class Revision
+    {
+        public int RevisionId { get; set; }
+        public int UserId { get; set; }
+        public string ProblemDetails { get; set; }
+        
+    }
+}
